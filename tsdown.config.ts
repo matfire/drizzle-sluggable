eec58d1b6622d062b37cb8f5a54955629577a35e
Matteo Gassend <mgassend@gmail.com>
2026-03-14 19:41:58 +0100

    fix: tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/pg/index.ts", "src/mysql/index.ts", "src/sqlite/index.ts"],
+  entry: [
+    "src/index.ts",
+    "src/pg/index.ts",
+    "src/mysql/index.ts",
+    "src/sqlite/index.ts",
+  ],
   format: "esm",
   dts: true,
   clean: true,
-  sourcemap: true,
   outDir: "./dist",
 });
